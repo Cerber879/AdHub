7ce406f97fdc6c212ad479e7d44bf815d42f0eca
Timur20832 <92998774+Timur20832@users.noreply.github.com>
2024-11-13 19:40:05 +0300

    add_about_us_page

---
+++ src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateChtoTo from '../../components/AboutUs/CreateChtoTo';
+
+const AboutUs: React.FC = () => {
+  return (
+    <div>
+      <CreateChtoTo/>
+      {/* Add more content here */}
+    </div>
+  );
+};
+
+export default AboutUs;
