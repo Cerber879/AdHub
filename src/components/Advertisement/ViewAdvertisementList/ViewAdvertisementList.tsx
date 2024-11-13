ef8eb4c552d646440e6f523f2929d6c39b1ef481
Fivii12 <dryg882@gmail.com>
2024-11-13 20:33:44 +0300

    class_to_view_adv

--- src/components/Advertisement/ViewAdvertisementList/ViewAdvertisementList.tsx
+++ src/components/Advertisement/ViewAdvertisementList/ViewAdvertisementList.tsx
@@ -32,7 +32,7 @@ const AdvertisementList: React.FC = () => {
           <button type="submit"></button>
         </form>
       </div>
-      <ul>
+      <ul className={styles.advertisment}>
         {advertisements.map((ad) => (
           <li key={ad.AdvertisementID}>{ad.Name}</li>
         ))}
