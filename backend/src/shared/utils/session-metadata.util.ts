ad192a6fe71cd64e3362384c33fe35574eae15b9
Cerber879 <nik.afansev789@mail.ru>
2024-12-31 21:18:43 +0300

    методы управления сессиями

--- backend/src/shared/utils/session-metadata.util.ts
+++ backend/src/shared/utils/session-metadata.util.ts
@@ -10,34 +10,49 @@ import DeviceDetector = require('device-detector-js')
 
 countries.registerLocale(require('i18n-iso-countries/langs/en.json'))
 
-export function getSessionMetadata(
-	req: Request,
-	userAgent: string
-): SessionMetadata {
-	const ip = IS_DEV_ENV
-		? '173.166.164.121'
-		: Array.isArray(req.headers['cf-connecting-ip'])
-			? req.headers['cf-connecting-ip'][0]
-			: req.headers['cf-connecting-ip'] ||
-				(typeof req.headers['x-forwarded-for'] === 'string'
-					? req.headers['x-forwarded-for'].split(',')[0]
-					: req.ip)
+import axios from 'axios';
 
-	const location = lookup(ip)
-	const device = new DeviceDetector().parse(userAgent)
+async function getPublicIP(): Promise<string> {
+	const response = await axios.get('https://api.ipify.org?format=json');
+	return response.data.ip; 
+}
 
-	return {
-		location: {
-			country: countries.getName(location.country, 'en') || 'Неизвестно',
-			city: location.city || 'Неизвестно',
-			latidute: location.ll[0] || 0,
-			longitude: location.ll[1] || 0
-		},
-		device: {
-			browser: device.client.name,
-			os: device.os.name,
-			type: device.device.type
-		},
-		ip
-	}
+export async function getSessionMetadata(
+    req: Request,
+    userAgent: string
+): Promise<SessionMetadata> {
+    const ip = IS_DEV_ENV
+        ? await getPublicIP() // Явное ожидание строки
+        : Array.isArray(req.headers['cf-connecting-ip'])
+            ? req.headers['cf-connecting-ip'][0]
+            : req.headers['cf-connecting-ip'] ||
+                (typeof req.headers['x-forwarded-for'] === 'string'
+                    ? req.headers['x-forwarded-for'].split(',')[0]
+                    : req.ip);
+
+    if (!ip || typeof ip !== 'string') {
+        throw new Error('IP-адрес не определён');
+    }
+
+    const location = lookup(ip); // Гарантированно строка
+    if (!location) {
+        throw new Error('Не удалось определить местоположение');
+    }
+
+    const device = new DeviceDetector().parse(userAgent);
+
+    return {
+        location: {
+            country: countries.getName(location.country, 'en') || 'Неизвестно',
+            city: location.city || 'Неизвестно',
+            latidute: location.ll[0] || 0,
+            longitude: location.ll[1] || 0
+        },
+        device: {
+            browser: device.client.name,
+            os: device.os.name,
+            type: device.device.type
+        },
+        ip
+    };
 }
