4a51bbc715718d5ebd83831bb69d8566903d6f51
Timur20832 <92998774+Timur20832@users.noreply.github.com>
2025-01-06 19:01:51 +0300

    инициализация

--- backend/prisma/generated/edge.js
+++ backend/prisma/generated/edge.js
@@ -226,7 +226,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\User\\source\\repos\\AdHub\\backend\\prisma\\generated",
+      "value": "C:\\Users\\user\\Desktop\\projects\\BD\\AdHub\\backend\\prisma\\generated",
       "fromEnvVar": null
     },
     "config": {
@@ -240,7 +240,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\User\\source\\repos\\AdHub\\backend\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\user\\Desktop\\projects\\BD\\AdHub\\backend\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
--- backend/prisma/generated/index.js
+++ backend/prisma/generated/index.js
@@ -227,7 +227,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\User\\source\\repos\\AdHub\\backend\\prisma\\generated",
+      "value": "C:\\Users\\user\\Desktop\\projects\\BD\\AdHub\\backend\\prisma\\generated",
       "fromEnvVar": null
     },
     "config": {
@@ -241,7 +241,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\User\\source\\repos\\AdHub\\backend\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\user\\Desktop\\projects\\BD\\AdHub\\backend\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
